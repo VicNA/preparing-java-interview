1427561a437b149ffcca90e411552426c79f78e5
viktor-n <vnikolaeff989@gmail.com>
2022-08-09 16:29:51 +0300

    Lesson 1 - task 3

---
+++ src/ru/geekbrains/lesson1/task3/Circle.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.lesson1.task3;
+
+public class Circle extends Figure {
+    @Override
+    public void area() {
+        System.out.println("Площадь круга");
+    }
+}
---
+++ src/ru/geekbrains/lesson1/task3/Square.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.lesson1.task3;
+
+public class Square extends Figure {
+    @Override
+    public void area() {
+        System.out.println("Площадь квадрата");
+    }
+}
