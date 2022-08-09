01b8048005ccff711cbcd5eb82fdbb4379eb52b8
viktor-n <vnikolaeff989@gmail.com>
2022-08-09 15:45:13 +0300

    Lesson 1 - task 1

---
+++ src/ru/geekbrains/lesson1/task1/Main.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.lesson1.task1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Person person1 = new Person.Builder().setFirstName("Viktor").setLastName("Nikolaev").build();
+        Person person2 = new Person.Builder().setFirstName("Viktor").setMiddleName("Alekseevich").build();
+
+        System.out.println(person1);
+        System.out.println(person2);
+    }
+}
---
+++ src/ru/geekbrains/lesson1/task1/Person.java
@@ -0,0 +1,114 @@
+package ru.geekbrains.lesson1.task1;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private String country;
+    private String address;
+    private String phone;
+    private String gender;
+
+    private Person(Builder builder) {
+        firstName = builder.firstName;
+        lastName = builder.lastName;
+        middleName = builder.middleName;
+        country = builder.country;
+        address = builder.address;
+        phone = builder.phone;
+        gender = builder.gender;
+    }
+
+//    public Person() {
+//    }
+
+    public static class Builder {
+        private String firstName;
+        private String lastName;
+        private String middleName;
+        private String country;
+        private String address;
+        private String phone;
+        private String gender;
+
+        public Builder setFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder setMiddleName(String middleName) {
+            this.middleName = middleName;
+            return this;
+        }
+
+        public Builder setCountry(String country) {
+            this.country = country;
+            return this;
+        }
+
+        public Builder setAddress(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public Builder setPhone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public Builder setGender(String gender) {
+            this.gender = gender;
+            return this;
+        }
+
+        public Person build() {
+            return new Person(this);
+        }
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", country='" + country + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
