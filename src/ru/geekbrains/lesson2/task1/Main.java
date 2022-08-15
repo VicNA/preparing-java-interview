eece12c6f13e203200b12046ba15fbe6e97928cd
VicN <vnikolaeff989@gmail.com>
2022-08-15 16:38:41 +0300

    Lesson 2 - task 1

---
+++ src/ru/geekbrains/lesson2/task1/Main.java
@@ -0,0 +1,10 @@
+package ru.geekbrains.lesson2.task1;
+
+import java.util.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(-11 >> 11);
+    }
+}
