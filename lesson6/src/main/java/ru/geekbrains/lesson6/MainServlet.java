ce8fc6f381f0f79e039f85fe0a120f16f690621c
viktor-n <vnikolaeff989@gmail.com>
2022-09-01 17:14:32 +0300

    Lesson 6 - Update task

--- lesson6/src/main/java/ru/geekbrains/lesson6/MainServlet.java
+++ lesson6/src/main/java/ru/geekbrains/lesson6/MainServlet.java
@@ -1,16 +1,20 @@
 package ru.geekbrains.lesson6;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet("/hello")
 public class MainServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter out = resp.getWriter();
-        out.print("<h1>Hello Servlet</h1>");
+        resp.setContentType("text/html");
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.write("Hello MainServlet!");
+        printWriter.close();
     }
 }
