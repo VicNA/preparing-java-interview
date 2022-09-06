5fda1c7f098b536bf6afd93521252c018f7a907e
viktor-n <vnikolaeff989@gmail.com>
2022-09-06 16:18:23 +0300

    Lesson 7 - Update task 1

--- lesson7/src/main/java/ru/geekbrains/lesson7/controllers/StudentController.java
+++ lesson7/src/main/java/ru/geekbrains/lesson7/controllers/StudentController.java
@@ -3,11 +3,22 @@ package ru.geekbrains.lesson7.controllers;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.geekbrains.lesson7.entities.Student;
 import ru.geekbrains.lesson7.services.StudentService;
+
+import java.util.List;
 
 @Controller
 @Setter
+@RequestMapping(path = "/stud")
 public class StudentController {
     @Autowired
     private StudentService studentService;
+
+    @GetMapping("/students")
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
 }
--- lesson7/src/main/java/ru/geekbrains/lesson7/entities/Student.java
+++ lesson7/src/main/java/ru/geekbrains/lesson7/entities/Student.java
@@ -1,4 +1,26 @@
 package ru.geekbrains.lesson7.entities;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "students")
+@NoArgsConstructor
+@Setter
+@Getter
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "age")
+    private Integer age;
 }
